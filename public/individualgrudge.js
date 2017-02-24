bdf43924183c36eeb92b4368e44773735f35609a
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-23 20:40:30 -0700

    Fixes bugs and lints

--- public/individualgrudge.js
+++ public/individualgrudge.js
@@ -2,35 +2,35 @@ const burnDetails = $('.individual-grudge');
 
 $(document).ready(function() {
   const id = parseInt(window.location.pathname.split("/")[1]);
-  getIndividualGrudgeFromServer(id)
+  getIndividualGrudgeFromServer(id);
 })
 
 const getIndividualGrudgeFromServer = (id) => {
   axios.get(`/api/grudges/${id}`)
   .then(response => {
-    let grudge = response.data
-    appendGrudge(grudge)
+    let grudge = response.data;
+    appendGrudge(grudge);
   })
 }
 
 const updateForgivenessStatus = (id) => {
   axios.put(`/api/grudges/${id}`)
   .then(res => {
-    clearDetails()
-    getIndividualGrudgeFromServer(id)
+    clearDetails();
+    getIndividualGrudgeFromServer(id);
   })
 }
 
 const clearDetails = () => {
-  $('.individual-grudge').html('')
+  burnDetails.html('');
 }
 
 const appendGrudge = (grudge) => {
-    let name = grudge.grudge.name
-    let offense = grudge.grudge.offense
-    let date = grudge.grudge.date
-    let status = grudge.grudge.status
-    let id = grudge.grudge.id
+    let name = grudge.grudge.name;
+    let offense = grudge.grudge.offense;
+    let date = grudge.grudge.date;
+    let status = grudge.grudge.status;
+    let id = grudge.grudge.id;
     burnDetails.append(`<article class="individual-grudge">
       <h4>
         <span class="burn-category" class="name">
