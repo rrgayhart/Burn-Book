80ad9d357d7de8a8dcd8176ecd9df98299de9902
Chelsea Skovgaard <chelseaskovgaard@gmail.com>
2017-02-23 19:19:08 -0700

    Adds functional to change forgiveness status on click

--- public/individualgrudge.js
+++ public/individualgrudge.js
@@ -1,32 +1,44 @@
-const id = parseInt(window.location.pathname.split("/")[1]);
+const burnDetails = $('.individual-grudge');
 
 $(document).ready(function() {
+  const id = parseInt(window.location.pathname.split("/")[1]);
+  console.log(id);
   getIndividualGrudgeFromServer(id)
 })
 
-$('.forgive-btn').on('click', (e) => {
-  e.preventDefault();
-  updateForgivenessStatus(id);
-});
-
-const updateForgivenessStatus = (id) => {
-  axios.put('/api/grudges/${id}')
-}
-
 const getIndividualGrudgeFromServer = (id) => {
-  axios.get('/api/grudges/${id}')
+  axios.get(`/api/grudges/${id}`)
   .then(response => {
     let grudge = response.data
     appendGrudge(grudge)
   })
 }
 
-const appendGrudges = (grudge) => {
-    let offense = grudge.offense
-    let date = grudge.date
-    let status = grudge.status
-    let id = grudge.id
-    burnList.append(`<article class="burn-details">
+const updateForgivenessStatus = (id) => {
+  axios.put(`/api/grudges/${id}`)
+  .then(res => {
+    clearDetails()
+    getIndividualGrudgeFromServer(id)
+  })
+}
+
+const clearDetails = () => {
+  $('.individual-grudge').html('')
+}
+
+const appendGrudge = (grudge) => {
+    let name = grudge.grudge.name
+    let offense = grudge.grudge.offense
+    let date = grudge.grudge.date
+    let status = grudge.grudge.status
+    let id = grudge.grudge.id
+    burnDetails.append(`<article class="individual-grudge">
+      <h4>
+        <span class="burn-category" class="name">
+          Name:
+        </span>
+        ${name}
+      </h4>
       <p>
         <span class="burn-category">
           Offense:
@@ -40,9 +52,9 @@ const appendGrudges = (grudge) => {
         ${date}
       </p>
       <p>
-        <span class="burn-category"> Forgiveness Status: </span>
+        <span class="burn-category"> Forgiven: </span>
         ${status}
       </p>
+      <input onClick="updateForgivenessStatus(${id})" type="submit"/>
       </article>`)
   }
-}
