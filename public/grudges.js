07b429b82000cc4f2c3066595f42bbc3ce7c5a5f
rrgayhart <rrgayhart@gmail.com>
2017-02-27 09:00:54 -0700

    Add an iffy

--- public/grudges.js
+++ public/grudges.js
@@ -1,12 +1,16 @@
-function returnCounts(grudges=[]){
-  let total = grudges.length;
-  let forgiven = grudges.filter(grudges => grudges.status === true).length;
-  let unforgiven = grudges.filter(grudges => grudges.status !== true).length;
-  return { total, forgiven, unforgiven };
-}
-
-if(typeof module !== 'undefined') {
-  module.exports = {
-   returnCounts
+(function(){
+  function returnCounts(grudges=[]){
+    let total = grudges.length;
+    let forgiven = grudges.filter(grudges => grudges.status === true).length;
+    let unforgiven = grudges.filter(grudges => grudges.status !== true).length;
+    return { total, forgiven, unforgiven };
   }
-}
+
+  if(typeof module !== 'undefined' && typeof module.exports !== 'undefined'){
+    module.exports = {
+     returnCounts
+    }
+  } else {
+      window.returnCounts = returnCounts
+  }
+})();
